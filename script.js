3b2f28be7f50fcd0f6bcbae9219af1d4c420e680
Your Name <didiabri@yahoo.com>
2023-02-20 14:54:13 +0800

    Add event listeners for number buttons and operator buttons, implement functions to append number and set operation respectively.

--- script.js
+++ script.js
@@ -2,40 +2,26 @@ const lastOperation = document.querySelector('.lastOpp');
 const currentOperation = document.querySelector('.currentOpp');
 const numberButtons = document.querySelectorAll('[data-key]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
-const decimalButtons = document.querySelector('[data-decimal');
-
-let firstOperation = 0;
-let secondOperation = 0;
-let currentOpe = null;
+const decimalButtons = document.querySelector('[data-decimal]');
+const refreshButton = document.querySelector('.clear');
 
 numberButtons.forEach(button => {
     button.addEventListener('click', function() {
-      console.log(button)
       appendNumber(button.textContent);
     });
   });
 
 operatorButtons.forEach(button => {
     button.addEventListener('click', function() {
-      console.log(button)
-     setOperation(button.textContent)
+     setOperation(button.textContent);
     });
   });
 
-function appendNumber (num) {
-  if (firstOperation === 0) {
-    firstOperation = num;
-  } else {
-    firstOperation += num;
+  function appendNumber (num) {
+    currentOperation.textContent = '';  
+    currentOperation.textContent += num;
   }
 
-  currentOperation.textContent = firstOperation;
-  console.log('current operation variable is ', currentOperation)
-}
-
-function setOperation (e) {
-  currentOpe = e;
-  lastOperation.textContent = `${firstOperation} ${currentOpe}`
-  firstOperation = 0;
-}
-
+  function setOperation (e) {
+    currentOperation.textContent = e
+  }
